e79012f948efc0b5ff4d26cd550a095432ee4725
shchae7_server <shchae7@postech.ac.kr>
2021-05-14 09:03:54 +0900

    neurifyBaB select added

--- src/custom/neurifyBaB.jl
+++ src/custom/neurifyBaB.jl
@@ -11,7 +11,29 @@ function solve(solver::NeurifyBaB, problem::Problem)
 
     nnet, output = problem.network, problem.output
     reach_list = []
-    print(nnet)
+    domain = init_symbolic_grad(problem.input)
+    splits = Set()
+
+    println("Domain")
+    println(domain)
+
+    for i in 1:solver.max_iter
+        if i > 1
+            domain, splits = select!(reach_list, solver.tree_search)
+            print(splits)
+        end
+    end
 
     return CounterExampleResult(:unknown)
+end
+
+function select!(reach_list, tree_search)
+    if tree_search == :BFS
+        reach = popfirst!(reach_list)
+    elseif tree_search == :DFS
+        reach = pop!(reach_list)
+    else
+        throw(ArgumentError(":$tree_search is not a valid tree search strategy"))
+    end
+    return reach
 end
